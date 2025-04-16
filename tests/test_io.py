15cb1d7ea0280b8c4477b0cf6d6013151c37abbd
carissableker <carissableker@users.noreply.github.com>
2025-04-16 09:04:37 +0200

    add io tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,24 +2,116 @@ import os
 import sys
 import unittest
 
+import networkx as nx
+import igraph as ig
 
+from booldog.io import read
 
-sys.path.append("/home/cbleker/research/NIB/squad/BoolDoG")
-from booldog.utils.io import read_boolean_graph, write_boolean_graph
-
+example_primes_true = {
+    'node_A': [
+        [{'node_B': 0, 'node_E': 0}, {'node_B': 1, 'node_E': 1}],
+        [{'node_B': 0, 'node_E': 1}, {'node_B': 1, 'node_E': 0}]
+    ],
+    'node_B': [
+        [{'node_B': 0}],
+        [{'node_B': 1}]
+    ],
+    'node_C': [
+        [{'node_B': 0}, {'node_A': 0}],
+        [{'node_A': 1, 'node_B': 1}]], 'node_D': [[{'node_D': 0}], [{'node_D': 1}]
+    ],
+    'node_E': [
+        [{'node_C': 1, 'node_D': 0}],
+        [{'node_D': 1}, {'node_C': 0}]
+    ]
+}
+example_primes_squad = {
+    'node_A': [
+        [{}],
+        []
+    ],
+    'node_B': [
+        [{'node_B': 0}],
+        [{'node_B': 1}]
+    ],
+    'node_C': [
+        [{'node_A': 0, 'node_B': 0}],
+        [{'node_B': 1}, {'node_A': 1}]
+    ],
+    'node_D': [
+        [{'node_D': 0}], [{'node_D': 1}]
+    ],
+    'node_E': [
+        [{'node_D': 0}, {'node_C': 1}],
+        [{'node_C': 0, 'node_D': 1}]
+    ]
+}
+example_interactions = {
+    'node_A': {
+        'node_C': '+'
+        },
+    'node_B': {
+        'node_A': '-',
+        'node_B': '+',
+        'node_C': '+'
+    },
+    'node_C': {
+        'node_E': '-'
+    },
+    'node_D': {
+        'node_D': '+',
+        'node_E': '+'
+    },
+    'node_E': {
+        'node_A': '-'
+    }
+}
+example_edgelist = [(s, t, example_interactions[s][t]) for s in example_interactions for t in example_interactions[s]]
+example_dict_of_dict = {s:{t:{"interaction": example_interactions[s][t]}} for s in example_interactions for t in example_interactions[s]}
 
 class Test(unittest.TestCase):
 
-    def test_read_boolean_graph(self):
-        pass
+    def test_read_bnet(self):
+        G = read("data/example.bnet", "bnet")
+        self.assertDictEqual(G.primes, example_primes_true)
 
+    def test_read_primes(self):
+        G = read("data/example.primes.json", "primes")
+        self.assertDictEqual(G.primes, example_primes_true)
 
-    def test_write_boolea_graph(self):
-        pass
+    def test_read_sbmlqual(self):
+        G = read("data/example.xml", "sbml-qual")
+        self.assertDictEqual(G.primes, example_primes_true)
 
+    ##################################################
+    # Uses SQUAD to turn into interactions Boolean Graph
+    ##################################################
 
+    def test_read_sif(self):
+        G = read("data/example.sif", "sif", activator_value="1", inhibitor_value="-1")
+        self.assertDictEqual(G.primes, example_primes_squad)
+
+    def test_read_interactions(self):
+        G = read(example_interactions, "interactions", activator_value="+", inhibitor_value="-")
+        self.assertDictEqual(G.primes, example_primes_squad)
+
+    def test_read_graphml(self):
+        G = read("data/example.graphml", "graphml")
+        self.assertDictEqual(G.primes, example_primes_squad)
+
+    def test_read_graphml_yEd(self):
+        G = read("data/example.yEd.graphml", "graphml", yEd=True, use_labels=False)
+        self.assertDictEqual(G.primes, example_primes_squad)
+
+    def test_read_igraph(self):
+        graph = ig.Graph.TupleList(example_edgelist, directed=True, edge_attrs=["weight"])
+        G = read(graph, "igraph", activator_value="+", inhibitor_value="-")
+        self.assertDictEqual(G.primes, example_primes_squad)
+
+    def test_read_networkx(self):
+        graph = nx.DiGraph(example_dict_of_dict)
+        G = read(graph, "networkx", activator_value="+", inhibitor_value="-", edge_type_key="interaction")
+        self.assertDictEqual(G.primes, example_primes_squad)
 
 if __name__ == '__main__':
     unittest.main()
-
-
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,10 +4,9 @@ import unittest
 
 import numpy as np
 
-
 sys.path.append("/home/cbleker/research/NIB/squad/BoolDoG")
-from booldog.utils.utils import ensure_ndarray, parameter_to_array
-
+from booldog.utils.misc import ensure_ndarray, parameter_to_array
+from booldog.utils import functions2mindnf
 
 class Test(unittest.TestCase):
 
@@ -34,8 +33,41 @@ class Test(unittest.TestCase):
 
             self.assertTrue(np.array_equal(result, parameter_to_array(obj, graph_keys), equal_nan=True))
 
-    def test_file_writeable(self):
-        pass
+    def test_functions2mindnf(self):
+        '''Based on pyboolnet tests '''
+
+        functions = {"x": lambda x, y: x or y, "y": lambda x, z: not x and z, "z": lambda x, y, z: sum([x, y, z]) == 1}
+        mindnf = functions2mindnf(functions)
+        self.assertDictEqual(mindnf, {'x': 'y | x', 'y': 'z&!x', 'z': 'z&!y&!x | !z&!y&x | !z&y&!x'})
+
+        # test the edit
+        matrix = np.array([[1, 0, 0],
+                           [1, 0, 1],
+                           [0, 0, 1]])
+
+        n = matrix.shape[0]
+        idx = {f"node{x}":x for x in range(n)}
+
+        def function_factory(data, node):
+            # actual node dependencies
+            args = [f"node{x}" for x in np.nonzero(data[idx[node]])[0]]
+
+            def func(*func_input):
+                # derive the network state from the input
+                network_state = [0]*n
+                for other_node, other_node_state in zip(args, func_input):
+                    network_state[idx[other_node]] = other_node_state
+
+                # calculate the node state
+                node_state = np.matmul(data[idx[node]], network_state)
+                return node_state
+            func.depends = args
+            return func
+
+        functions = {node:function_factory(matrix,  node) for node in idx.keys()}
+        mindnf = functions2mindnf(functions)
+
+        self.assertDictEqual(mindnf, {'node0': 'node0', 'node1': 'node2 | node0', 'node2': 'node2'})
 
 if __name__ == '__main__':
     unittest.main()
